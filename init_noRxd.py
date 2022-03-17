6486eb7d3e395fc15af95b1b3357d1c305368a78
craig <kelley92@bu.edu>
2022-03-17 12:38:33 -0400

    no rxd

--- init_noRxd.py
+++ init_noRxd.py
@@ -1,6 +1,6 @@
 from netpyne import sim
-from netParams import netParams
-from cfg import cfg
+from netParams_noRxd import netParams
+from cfg_noRxd import cfg
 import numpy as np
 import os 
 import sys
@@ -15,7 +15,7 @@ sim.net.createPops()                  # instantiate network populations
 sim.net.createCells()                 # instantiate network cells based on defined populations
 sim.net.connectCells()                # create connections between cells based on params
 sim.net.addStims()                    # add external stimulation to cells (IClamps etc)
-sim.net.addRxD(nthreads=6)    # add reaction-diffusion (RxD)
+# sim.net.addRxD(nthreads=6)    # add reaction-diffusion (RxD)
 sim.setupRecording()             # setup variables to record for each cell (spikes, V traces, etc)
 sim.simulate()
 sim.analyze()
