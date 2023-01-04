b3e30a869243ba0571971fb93d67c461a8d899f5
craig <kelley92@bu.edu>
2023-01-04 12:42:15 -0500

    reduced weight, increased freq

--- PDCM_RxD/netParamsRxd.py
+++ PDCM_RxD/netParamsRxd.py
@@ -146,9 +146,9 @@ netParams.synMechParams['exc'] = {'mod': 'Exp2Syn', 'tau1': 0.8, 'tau2': 5.3, 'e
 netParams.synMechParams['inh'] = {'mod':'Exp2Syn', 'tau1': 0.07, 'tau2': 18.2, 'e': -80} # GABAA
 
 # added bkg inputs 
-netParams.stimSourceParams['bkg'] = {'type': 'NetStim', 'rate': 4, 'noise': 0.3, 'start' : 200}
-netParams.stimTargetParams['bkg->L4e'] = {'source': 'bkg', 'conds': {'pop': ['L4e'], 'cellList' : [i for i in range(100)]}, 'weight': 0.1, 'delay': 1, 'synMech': 'exc'}
-netParams.stimTargetParams['bkg->L4i'] = {'source': 'bkg', 'conds': {'pop': ['L4i'], 'cellList' : [i for i in range(50)]}, 'weight': 0.1, 'delay': 1, 'synMech': 'exc'}
+netParams.stimSourceParams['bkg'] = {'type': 'NetStim', 'rate': 5, 'noise': 0.3, 'start' : 100}
+netParams.stimTargetParams['bkg->L4e'] = {'source': 'bkg', 'conds': {'pop': ['L4e'], 'cellList' : [i for i in range(100)]}, 'weight': 0.05, 'delay': 1, 'synMech': 'exc'}
+netParams.stimTargetParams['bkg->L4i'] = {'source': 'bkg', 'conds': {'pop': ['L4i'], 'cellList' : [i for i in range(50)]}, 'weight': 0.05, 'delay': 1, 'synMech': 'exc'}
 
 # if cfg.DC == False: # External Input as Poisson
 #     for r in range(0,8):
