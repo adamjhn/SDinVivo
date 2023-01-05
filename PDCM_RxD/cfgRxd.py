ac0c5daf104042c627265aea0b21c43cdb798c06
craig <kelley92@bu.edu>
2023-01-05 09:51:33 -0500

    run longer

--- PDCM_RxD/cfgRxd.py
+++ PDCM_RxD/cfgRxd.py
@@ -8,13 +8,13 @@ import numpy as np
 
 # Run parameters
 cfg = specs.SimConfig()       # object of class cfg to store simulation configuration
-cfg.duration = 0.5e3 #2e3        # Duration of the simulation, in ms
+cfg.duration = 5e3 #2e3        # Duration of the simulation, in ms
 cfg.hParams['v_init'] = -70.0   # set v_init to -65 mV
 cfg.hParams['celsius'] = 37.0
 cfg.dt = 0.025 #0.025              # Internal integration timestep to use
 cfg.verbose = False            # Show detailed messages 
 cfg.recordStep = 1             # Step size in ms to save data (eg. V traces, LFP, etc)
-cfg.filename = 'Data/reducedConn001_bkg2allL4_500ms/'   # Set file output name
+cfg.filename = 'Data/reducedConn001_bkg2allL4_5s/'   # Set file output name
 cfg.printPopAvgRates = True
 cfg.printRunTime = 1
 cfg.Kceil = 15.0
