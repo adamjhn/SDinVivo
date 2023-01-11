4bff0f788c2d32e9d3259f9e2751e349402140f0
craig <kelley92@bu.edu>
2023-01-11 11:30:25 -0500

    1.5x

--- PDCM_RxD/netParamsRxd.py
+++ PDCM_RxD/netParamsRxd.py
@@ -223,7 +223,7 @@ for r in range(0,8):
             connFactor = cfg.connFactor
         else:
             syn = 'inh'
-            connFactor = '2*' + cfg.connFactor
+            connFactor = '1.5*' + cfg.connFactor
         if (c % 2) == 0:
             if c == 2 and r == 0:
                 netParams.connParams[str(L[c])+'->'+str(L[r])] = { 
