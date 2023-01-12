c3aabf58eac196463c3802f260d3248cfe52b258
craig <kelley92@bu.edu>
2023-01-12 11:11:27 -0500

    2s, scalledConnWeight=1e-6

--- PDCM_RxD/cfgRxd.py
+++ PDCM_RxD/cfgRxd.py
@@ -8,17 +8,17 @@ import numpy as np
 
 # Run parameters
 cfg = specs.SimConfig()       # object of class cfg to store simulation configuration
-cfg.duration = 0.5e3        # Duration of the simulation, in ms
+cfg.duration = 2e3        # Duration of the simulation, in ms
 cfg.hParams['v_init'] = -70.0   # set v_init to -65 mV
 cfg.hParams['celsius'] = 37.0
 cfg.dt = 0.025 #0.025              # Internal integration timestep to use
 cfg.verbose = False            # Show detailed messages 
 cfg.recordStep = 1             # Step size in ms to save data (eg. V traces, LFP, etc)
-cfg.filename = 'Data/test_scaleConnWeight/'   # Set file output name
+cfg.filename = 'Data/scaleConnWeight1e-6_poissonInputs_2s/'   # Set file output name
 cfg.printPopAvgRates = True
 cfg.printRunTime = 1
 cfg.Kceil = 15.0
-cfg.nRec = 40
+cfg.nRec = 120
 cfg.recordCellsSpikes = ['L2e', 'L2i', 'L4e', 'L4i', 'L5e', 'L5i','L6e', 'L6i'] # record only spikes of cells (not ext stims)
 
  # Network dimensions
