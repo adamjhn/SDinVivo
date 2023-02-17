ea25c38a398b93ab273f2618ffee0f016f8c1e58
craig <kelley92@bu.edu>
2023-02-17 17:19:25 -0500

    change netpyne path for neurosim

--- PDCM_RxD/initCaps.py
+++ PDCM_RxD/initCaps.py
@@ -1,5 +1,6 @@
 import sys 
-sys.path.insert(0,'/home/ckelley/netpyne/')
+# sys.path.insert(0,'/home/ckelley/netpyne/')
+sys.path.insert(0, '/u/craig/netpyne/')
 from netpyne import sim
 from netParamsCaps import netParams
 from cfgCaps import cfg
